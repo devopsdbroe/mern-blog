8773e089582777e57116a3fdef1a0fea2b218b47
devopsdbroe <dbroe1995@gmail.com>
2024-04-13 13:10:55 -0400

    Create routes for sign-in and sign-up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
-	return <Navbar />;
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Navbar />}
+			>
+				<Route
+					path="signin"
+					element={<h1>Sign In Page</h1>}
+				/>
+				<Route
+					path="signup"
+					element={<h1>Sign Up Page</h1>}
+				/>
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
