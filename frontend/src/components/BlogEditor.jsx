46eec34e606e4af76bc6aa6709fefdc8c3be5925
Daniel Broe <dbroe1995@gmail.com>
2024-04-19 12:59:25 -0400

    Create basic title input field for blog editor page

--- frontend/src/components/BlogEditor.jsx
+++ frontend/src/components/BlogEditor.jsx
@@ -1,13 +1,18 @@
-import { useRef } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import defaultBanner from "../assets/blog banner.png";
 import AnimationWrapper from "./AnimationWrapper";
 import { uploadImage } from "../services/aws";
 import { Toaster, toast } from "react-hot-toast";
+import { EditorContext } from "../pages/Editor";
 
 const BlogEditor = () => {
-	const imgRef = useRef();
+	const {
+		blog,
+		blog: { title, banner, content, tags, description },
+		setBlog,
+	} = useContext(EditorContext);
 
 	const handleBannerUpload = (e) => {
 		const img = e.target.files[0];
@@ -21,7 +26,7 @@ const BlogEditor = () => {
 						toast.dismiss(loadingToast);
 						toast.success("Upload Successful");
 
-						imgRef.current.src = url;
+						setBlog({ ...blog, banner: url });
 					}
 				})
 				.catch((err) => {
@@ -31,17 +36,39 @@ const BlogEditor = () => {
 		}
 	};
 
+	const handleError = (e) => {
+		let img = e.target;
+
+		img.src = defaultBanner;
+	};
+
+	// Prevent user from creating a new line for title
+	const handleTitleKeyDown = (e) => {
+		// Check for "Enter" key code
+		if (e.keyCode === 13) {
+			e.preventDefault();
+		}
+	};
+
+	const handleTitleChange = (e) => {
+		// Store reference to textarea
+		let input = e.target;
+
+		input.style.height = "auto";
+		input.style.height = input.scrollHeight + "px";
+
+		setBlog({ ...blog, title: input.value });
+	};
+
 	return (
 		<>
 			<nav className="navbar">
 				<Link to="/">
-					<img
-						src={Logo}
-						alt="logo"
-						className="flex-none w-10"
-					/>
+					<img src={Logo} alt="logo" className="flex-none w-10" />
 				</Link>
-				<p className="max-md:hidden text-black line-clamp-1 w-full">New blog</p>
+				<p className="max-md:hidden text-black line-clamp-1 w-full">
+					{title.length ? title : "New Blog"}
+				</p>
 
 				<div className="flex gap-4 ml-auto">
 					<button className="btn-dark py-2">Publish</button>
@@ -55,10 +82,10 @@ const BlogEditor = () => {
 						<div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
 							<label htmlFor="uploadBanner">
 								<img
-									ref={imgRef}
-									src={defaultBanner}
+									src={banner}
 									alt="banner image"
 									className="z-20"
+									onError={handleError}
 								/>
 								<input
 									id="uploadBanner"
@@ -69,6 +96,15 @@ const BlogEditor = () => {
 								/>
 							</label>
 						</div>
+
+						<textarea
+							placeholder="Blog Title"
+							className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+							onKeyDown={handleTitleKeyDown}
+							onChange={handleTitleChange}
+						></textarea>
+
+						<hr className="w-full opacity-10 my-5" />
 					</div>
 				</section>
 			</AnimationWrapper>
--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1,22 +1,44 @@
-import { useState, useContext } from "react";
+import { useState, useContext, createContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
 import BlogEditor from "../components/BlogEditor";
 import PublishForm from "../components/PublishForm";
 
+// Define starting state of editor
+const blogStructure = {
+	title: "",
+	banner: "",
+	content: [],
+	tags: [],
+	description: "",
+	author: {
+		personal_info: {},
+	},
+};
+
+export const EditorContext = createContext({});
+
 const Editor = () => {
 	const [editorState, setEditorState] = useState("editor");
+
+	const [blog, setBlog] = useState(blogStructure);
 
 	const {
 		userAuth: { access_token },
 	} = useContext(UserContext);
 
-	return access_token === null ? (
-		<Navigate to="/signin" />
-	) : editorState === "editor" ? (
-		<BlogEditor />
-	) : (
-		<PublishForm />
+	return (
+		<EditorContext.Provider
+			value={{ blog, setBlog, editorState, setEditorState }}
+		>
+			{access_token === null ? (
+				<Navigate to="/signin" />
+			) : editorState === "editor" ? (
+				<BlogEditor />
+			) : (
+				<PublishForm />
+			)}
+		</EditorContext.Provider>
 	);
 };
 export default Editor;
