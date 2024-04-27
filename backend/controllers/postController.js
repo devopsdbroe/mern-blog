bdf7b0d54db83764ee1984fd07fa62367eabf799
devopsdbroe <dbroe1995@gmail.com>
2024-04-27 15:15:57 -0400

    Add route for fetching trending blog posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -71,3 +71,24 @@ export const getLatestBlogs = (req, res) => {
 			return res.status(500).json({ error: err.message });
 		});
 };
+
+export const getTrendingBlogs = (req, res) => {
+	Blog.find({ draft: false })
+		.populate(
+			"author",
+			"personal_info.fullname personal_info.username personal_info.profile_img -_id"
+		)
+		.sort({
+			"activity.total_reads": -1,
+			"activity.total_likes": -1,
+			publishedAt: -1,
+		})
+		.select("blog_id title publishedAt -_id")
+		.limit(5)
+		.then((blogs) => {
+			return res.status(200).json({ blogs });
+		})
+		.catch((err) => {
+			return res.status(500).json({ error: err.message });
+		});
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { createBlog, getLatestBlogs } from "../controllers/postController.js";
+import {
+	createBlog,
+	getLatestBlogs,
+	getTrendingBlogs,
+} from "../controllers/postController.js";
 import {
 	validateBlogData,
 	validateJWT,
@@ -9,5 +13,6 @@ const router = express.Router();
 
 router.post("/createBlog", validateJWT, validateBlogData, createBlog);
 router.get("/getLatestBlogs", getLatestBlogs);
+router.get("/getTrendingBlogs", getTrendingBlogs);
 
 export default router;
