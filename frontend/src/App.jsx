4e060a0c8f37d008d444ca3cfde5fd737cee25c9
Daniel Broe <dbroe1995@gmail.com>
2024-05-08 14:24:46 -0400

    Implement search route for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,18 @@ import UserAuth from "./pages/UserAuth";
 import { UserProvider } from "./context/UserProvider";
 import Editor from "./pages/Editor";
 import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
 	return (
 		<UserProvider>
 			<Routes>
-				<Route
-					path="/editor"
-					element={<Editor />}
-				/>
-				<Route
-					path="/"
-					element={<Navbar />}
-				>
-					<Route
-						index
-						element={<Home />}
-					/>
-					<Route
-						path="signin"
-						element={<UserAuth type="sign-in" />}
-					/>
-					<Route
-						path="signup"
-						element={<UserAuth type="sign-up" />}
-					/>
+				<Route path="/editor" element={<Editor />} />
+				<Route path="/" element={<Navbar />}>
+					<Route index element={<Home />} />
+					<Route path="signin" element={<UserAuth type="sign-in" />} />
+					<Route path="signup" element={<UserAuth type="sign-up" />} />
+					<Route path="search/:query" element={<SearchPage />} />
 				</Route>
 			</Routes>
 		</UserProvider>
