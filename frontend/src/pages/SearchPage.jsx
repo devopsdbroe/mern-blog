6cc36f9741b6164e80c5823ca2045521be35da9a
devopsdbroe <dbroe1995@gmail.com>
2024-05-10 17:21:35 -0400

    Implement user cards for search results

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -8,6 +8,7 @@ import BlogCard from "../components/BlogCard";
 import NoDataMessage from "../components/NoDataMessage";
 import LoadMoreDataBtn from "../components/LoadMoreDataBtn";
 import { filterPaginationData } from "../components/FilterPaginationData";
+import UserCard from "../components/UserCard";
 
 const SearchPage = () => {
 	const { query } = useParams();
@@ -63,37 +64,68 @@ const SearchPage = () => {
 		fetchUsers();
 	}, [query]);
 
+	const UserCardWrapper = () => {
+		return (
+			<>
+				{users === null ? (
+					<Loader />
+				) : users.length ? (
+					users.map((user, i) => (
+						<AnimationWrapper
+							key={i}
+							transition={{ duration: 1, delay: i * 0.08 }}
+						>
+							<UserCard user={user} />
+						</AnimationWrapper>
+					))
+				) : (
+					<NoDataMessage message="No users found" />
+				)}
+			</>
+		);
+	};
+
 	return (
 		<section className="h-cover flex justify-center gap-10">
 			<div className="w-full">
 				<InPageNavigation
 					routes={[`Search Results for "${query}"`, "Accounts Matched"]}
 					defaultHidden={["Accounts Matched"]}
-				/>
+				>
+					<>
+						{blogs === null ? (
+							<Loader />
+						) : blogs.results.length ? (
+							blogs.results.map((blog, i) => (
+								<AnimationWrapper
+									transition={{ duration: 1, delay: i * 0.1 }}
+									key={i}
+								>
+									<BlogCard
+										content={blog}
+										author={blog.author.personal_info}
+									/>
+								</AnimationWrapper>
+							))
+						) : (
+							<NoDataMessage message="No blogs found" />
+						)}
+						<LoadMoreDataBtn
+							state={blogs}
+							fetchData={searchBlogs}
+						/>
+					</>
 
-				<>
-					{blogs === null ? (
-						<Loader />
-					) : blogs.results.length ? (
-						blogs.results.map((blog, i) => (
-							<AnimationWrapper
-								transition={{ duration: 1, delay: i * 0.1 }}
-								key={i}
-							>
-								<BlogCard
-									content={blog}
-									author={blog.author.personal_info}
-								/>
-							</AnimationWrapper>
-						))
-					) : (
-						<NoDataMessage message="No blogs found" />
-					)}
-					<LoadMoreDataBtn
-						state={blogs}
-						fetchData={searchBlogs}
-					/>
-				</>
+					<UserCardWrapper />
+				</InPageNavigation>
+			</div>
+
+			<div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-gray pl-8 pt-3 max-md:hidden">
+				<h1 className="font-medium text-xl mb-8">
+					Users Related to Search <i className="fi fi-rr-user mt-1"></i>
+				</h1>
+
+				<UserCardWrapper />
 			</div>
 		</section>
 	);
